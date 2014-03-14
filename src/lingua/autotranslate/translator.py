bf30145ca0fc4877aa837aebbdf9682a1d95d82c
Daniel Widerin <daniel.widerin@zumtobelgroup.com>
2014-03-14 14:06:35 +0100

    configureable source language, add i18n-variable warning

--- src/lingua/autotranslate/translator.py
+++ src/lingua/autotranslate/translator.py
@@ -9,15 +9,20 @@ from translate import Translator
 
 
 DEFAULT = re.compile(r'Default:\s(.*)')
+VAR_REGEX = re.compile(r'(\$\{[^\}]*\})')
 SOURCE_LANGUAGE = 'en'
 BREATHE = 0.25
+ERROR = 31
+SUCCESS = 32
+INFO = 33
 
 
 def log(bash_color, type, text):
     print u'[\033[{0}m {1} \033[0m] {2}'.format(bash_color, type, text)
 
 
-def autotranslate(path, target_language, ignore_already_translated=True):
+def autotranslate(path, source_language, target_language,
+                  ignore_already_translated=True):
     """Given a po file which is opened using polib and processed through
        Google Translator for all untranslated items by default
     """
@@ -28,7 +33,7 @@ def autotranslate(path, target_language, ignore_already_translated=True):
         raise Exception(("Encoding problem while parsing {0}, are you "
                          "sure that's a PO file?").format(path))
 
-    translator = Translator(to_lang=target_language, from_lang=SOURCE_LANGUAGE)
+    translator = Translator(to_lang=target_language, from_lang=source_language)
     try:
         for idx, entry in enumerate(catalog):
 
@@ -41,35 +46,47 @@ def autotranslate(path, target_language, ignore_already_translated=True):
             else:
                 to_translate = entry.msgid
 
+            # Do we have to handle variables?
+            variables = VAR_REGEX.search(to_translate)
             try:
-                translated = translator.translate(to_translate)
+                translated = translator.translate(to_translate.encode('utf-8'))
             except urllib2.HTTPError as e:
-                log(31, 'Error', u'{0:s} raised {1}: {2:s}'.format(
+                log(ERROR, 'Error', u'{0:s} raised {1}: {2:s}'.format(
                     entry, e.__class__, e))
                 continue
 
-            log(32, 'Success', u'{0} -> {1}'.format(to_translate, translated))
+            if variables is not None:
+                log(INFO, 'Found variable(s)', ', '.join(variables.groups()))
+
+            log(SUCCESS, 'Success', u'{0} -> {1}'.format(
+                to_translate, translated))
 
             # Save entry
             catalog[idx].msgstr = translated
             time.sleep(BREATHE)
 
     except KeyboardInterrupt:
-        log(31, 'Quit', '')
+        log(ERROR, 'Quit', '')
 
     catalog.save()
 
 
 def AutoTranslator():
     parser = argparse.ArgumentParser(description='Translate Po files.')
-    parser.add_argument('-i', dest='input', action='append', nargs=2,
-                              required=True, metavar=('<locale>', '<po file>'),
-                              help='Locale and filename of po-file to process')
-    parser.add_argument('-u', dest='update', action='store_const',
-                              default=False, const=True,
-                              help='Force updating translations by '
-                                   'retranslating all msgids.')
+    parser.add_argument('-i',
+                        dest='input', action='append', nargs=2,
+                        required=True, metavar=('<locale>', '<po file>'),
+                        help='Locale and filename of po-file to process')
+    parser.add_argument('-s',
+                        dest='source_lang', type=str,
+                        default=SOURCE_LANGUAGE, metavar='LANGUAGE',
+                        help='Source language to translate from.')
+    parser.add_argument('-u',
+                        dest='update', action='store_true',
+                        help='Force updating translations by '
+                             'retranslating all msgids.')
     options = parser.parse_args()
 
-    for locale, pofile in options.input:
-        autotranslate(pofile, locale, not options.update)
+    for target_lang, pofile in options.input:
+        autotranslate(
+            pofile, options.source_lang, target_lang, not options.update)
